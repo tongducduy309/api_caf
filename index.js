f952b17ec8448cf5873349debec1702b43af3807
tongducduy309 <119146803+tongducduy309@users.noreply.github.com>
2024-10-20 11:06:13 +0700

    u

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ router.get('/get/products/all', (req, res) => {
 router.get('/get/products/:key', (req, res) => {
     const key = req.params.key;
     if (key!='all'){
-        const query = `SELECT * FROM PRODUCTS WHERE LOWER(name) LIKE '%${key}%'`
+        let query = `SELECT * FROM PRODUCTS WHERE LOWER(name) LIKE '%${key}%'`
         if (key.replaceAll(/\D/g, '')==key){
             query = `SELECT * FROM PRODUCTS WHERE LOWER(name) LIKE '%${key}%' OR id = ${key}`
         }
