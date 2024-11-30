5956ec73c8bfe9e4c085e69afddbc94a492abdd2
tongducduy309 <119146803+tongducduy309@users.noreply.github.com>
2024-11-30 10:57:31 +0700

    u

--- index.js
+++ index.js
@@ -688,9 +688,10 @@ router.get('/get/customer-reviews/:id', (req, res) => {
 
 router.get('/get/best-customer-reviews/:number', (req, res) => {
     const number = req.params.number;
+    
     pool.query(`SELECT p.*,IMG_PRODUCT.img FROM (select CS.*, products.name as p_name from (SELECT * FROM CUSTOMER_REVIEWS 
 ORDER BY point ASC
-LIMIT 2) as CS
+LIMIT ${number}) as CS
 LEFT JOIN PRODUCTS ON PRODUCTS.name_id=CS.name_id) as p
 LEFT JOIN IMG_PRODUCT ON IMG_PRODUCT.p_name_id=p.name_id`, (error, results) => {
         if (error) {
