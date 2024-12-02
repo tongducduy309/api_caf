303efd3da695ef9edf79cbc000456c57e15d4933
tongducduy309 <119146803+tongducduy309@users.noreply.github.com>
2024-12-02 09:04:23 +0700

    u

--- index.js
+++ index.js
@@ -784,34 +784,26 @@ router.post('/post/categories', (req, res) => {
 // ==========================CHECKOUT================================
 router.post('/post/checkout', async (req, res) => {
     const form = req.body;
-    // const uid=form.uid
     const bill=form.bill
     const user=form.user
 
+    let data = ''
+    const bid = bill.id
+    for (let s of bill.products){
+        data+=`('${bid}', '${s.id}', '${s.quantity}', '${s.price}', '${s.sale}' , '${s.note}'),`
+    }
+    data = data.slice(0, -1);
     
     pool.query(`INSERT INTO BILL (receiver,contactnumber,address,subtotal,delivery_fee,cost,discount,paymentmethod,payment_status) VALUES
-    ('${user.receiver}', '${user.contactnumber}', '${user.address}', '${bill.subtotal}', '${bill.delivery_fee}', '${bill.cost}', '${bill.discount}', '${bill.paymentmethod}', '${bill.payment_status}')`, (error, results) => {
+    ('${user.receiver}', '${user.contactnumber}', '${user.address}', '${bill.subtotal}', '${bill.delivery_fee}', '${bill.cost}', '${bill.discount}', '${bill.paymentmethod}', '${bill.payment_status}') ; INSERT INTO DETAIL_BILL (bid,pid,quantity,price,sale,note) VALUES `+data, async (error, results) => {
         if (error) {
             console.error(error);
             res.status(500).json({result:'error',message:error});
         } else {
-            let data = ''
-            // const bid = results.rows[0]
-            return res.json(results)
-            for (let s of bill.products){
-                data+=`('${bid}', '${s.id}', '${s.quantity}', '${s.price}', '${s.sale}' , '${s.note}'),`
-            }
-            data = data.slice(0, -1);
-            pool.query(`INSERT INTO DETAIL_BILL (bid,pid,quantity,price,sale,note) VALUES `+data, async (error, results) => {
-                    if (error) {
-                        console.error(error);
-                        res.status(500).json({result:'error',message:error});
-                    } else {
-                        await sendEmail_Order(user.email,user,bill)
-                        res.status(200).json({result:'success'});
-                    }
-                });
-            // res.status(200).json({result:'success'});
+            
+            
+            await sendEmail_Order(user.email,user,bill)
+            res.status(200).json({result:'success'});
         }
     });
     
