e87af2a0cca2c81be079b87e22bc8cc17f202253
tongducduy309 <119146803+tongducduy309@users.noreply.github.com>
2024-12-02 09:48:49 +0700

    u

--- index.js
+++ index.js
@@ -795,7 +795,7 @@ router.post('/post/checkout', async (req, res) => {
     data = data.slice(0, -1);
     
     pool.query(`INSERT INTO BILL (id,receiver,contactnumber,address,subtotal,delivery_fee,cost,discount,paymentmethod,payment_status) VALUES
-    ('${bill.id}','${user.receiver}', '${user.contactnumber}', '${user.address}', '${bill.subtotal}', '${bill.delivery_fee}', '${bill.cost}', '${bill.discount}', '${bill.paymentmethod}', '${bill.payment_status}') ; INSERT INTO DETAIL_BILL (bid,pid,quantity,price,sale,note) VALUES `+data, async (error, results) => {
+    ('${bid}','${user.receiver}', '${user.contactnumber}', '${user.address}', '${bill.subtotal}', '${bill.delivery_fee}', '${bill.cost}', '${bill.discount}', '${bill.paymentmethod}', '${bill.payment_status}') ; INSERT INTO DETAIL_BILL (bid,pid,quantity,price,sale,note) VALUES `+data, async (error, results) => {
         if (error) {
             console.error(error);
             res.status(500).json({result:'error',message:error});
